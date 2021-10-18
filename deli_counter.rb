1b5294e894a93f344832c08086c83904aae2f47e
Storm <storm.sarracino01@gmail.com>
2021-10-18 13:41:54 -0500

    commit tree d7a0be75313193217784b4ab62e2be7ec796e0a2

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def line(deli)
+   if deli.length == 0 
+      puts "The line is currently empty."
+   else
+      current_deli = "The line is currently:"
+      deli.map.with_index(1) do |customer, i|
+         current_deli << " #{i}. #{customer}"
+   end
+   puts current_deli
+end
+end
+
+def take_a_number(deli, name)
+   deli.push(name)
+   puts "Welcome, #{name}. You are number #{deli.length} in line."
+end 
+
+def now_serving(deli)
+   if deli.length == 0 
+      puts "There is nobody waiting to be served!"
+   else
+      puts "Currently serving #{deli.first}."
+      deli.shift
+   end
+end
